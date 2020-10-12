1f6c576b3396a934c60e5149bfa2b2e4b98ddbc3
Abiel Zulio M <abielzm@gmail.com>
2020-10-12 22:23:38 +0700

    add boil a egg

--- math.py
+++ math.py
@@ -1,12 +1,23 @@
 def narcissistic(n):
-  cacah = len(str(n))
-  listnarsis = []
-  for i in str(n):
-    narsis = int(i) ** cacah
-    listnarsis.append(int(narsis))
-  narsistik = sum(listnarsis)
 
-  if narsistik == int(n):
-    print(n, "termasuk bilangan narcissistic")
-  else:
-    print(n, "tidak termasuk bilangan narcissistic")
+    print("[NARCISSISTIC CHECKER PROGRAM]", "\n")
+    cacah = len(str(n))
+    listnarsis = []
+
+    for i in str(n):
+        narsis = int(i) ** cacah
+        listnarsis.append(int(narsis))
+        narsistik = sum(listnarsis)
+
+        if narsistik == int(n):
+            print(n, "is a narcissistic number.")
+        else:
+            print(n, "is not a narcissistic number.")
+
+def boilaegg(n):
+    print("[BOIL A EGG is a program to count how long to boil a eggs with 8 eggs per 5 minutes performance.]", "\n")
+    if int(n) == 0:
+        print("\n" + "You haven't input any egg yet." + "\n")
+    else:
+        minute = -(-int(n) // 8) * 5
+        print(n, "eggs for", minute, "minutes." + "\n")
